c9ba3ca68599aaff103dda8dd3bd1e7fbfb1e7f9
Cordon Fabien <fabien.cordon@ext.groupepvcp.com>
2019-08-28 14:27:15 +0200

    Intro component

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
-import chcLogo from '../img/logos/chc_logo.png'
+import backIntro from '../img/backgrounds/elans_loisir_1920x500.jpg'
 
 const Intro = () => {
   return (
     <div className='home-intro'>
-      <img src={chcLogo} alt='Puck on fire CHC' />
+      <img src={backIntro} alt='Les Elans loisir de Champigny' />
     </div>
   );
 }

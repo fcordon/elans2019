35865e0a75dad1c3a642b9bf540d07203cd04a30
Fabien Cordon <fabien.cordon@ext.groupepvcp.com>
2019-09-13 11:11:54 +0200

    build 1.0

---
+++ client/build/precache-manifest.452f622d2a4455276499899c28f517fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15fb5d6cb229cbd90be6ef1f1c175770",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c725c23ebd93124cf0a",
+    "url": "/static/css/main.ea9760c5.chunk.css"
+  },
+  {
+    "revision": "b565d553fc2435a6c159",
+    "url": "/static/js/2.ecb91131.chunk.js"
+  },
+  {
+    "revision": "3c725c23ebd93124cf0a",
+    "url": "/static/js/main.5800187e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "da33916eae4ea6baa425b5aec9fc57ae",
+    "url": "/static/media/elans_loisir_1920x500.da33916e.jpg"
+  },
+  {
+    "revision": "ed436a21ee185494c8f237d0ea98baad",
+    "url": "/static/media/loisirs_2018_2019.ed436a21.jpg"
+  }
+]);

46b490c1f57d9dd8f43260e740f70070039d73d2
Fabien Cordon <fabien.cordon@ext.groupepvcp.com>
2019-09-24 11:00:26 +0200

    SCSS refecto

--- client/src/App.js
+++ client/src/App.js
@@ -3,9 +3,6 @@ import React from 'react'
 import Menu from './components/Menu'
 import Content from './Content'
 
-import './scss/App.scss'
-import './scss/journal.min.css'
-
 const App = () => {
   return (
     <section className='App'>

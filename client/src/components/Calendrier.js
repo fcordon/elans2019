5721d572025e89a938401fa3d8cf43533b2f21c3
Cordon Fabien <fabien.cordon@ext.groupepvcp.com>
2019-09-27 14:01:05 +0200

    Page calendrier done

--- client/src/components/Calendrier.js
+++ client/src/components/Calendrier.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Row, Col } from 'react-bootstrap'
+import { Row, Col, Card } from 'react-bootstrap'
 
 const Calendrier = (props) => {
   const [gameDate, setGameDate] = useState('')
@@ -18,35 +18,39 @@ const Calendrier = (props) => {
   }, [props.gameDate])
 
   return (
-    <section className='home-calendrier'>
-      <Col xs={12} className='home-calendrier-date'>
-        {gameDate}
-      </Col>
-      <Col xs={12} className='home-calendrier-equipe'>
-        <Row>
-          <Col className='home-calendrier-equipe-img' xs={4}>
-            <img src={props.patinoire === 'home' ? '/equipes/logo-'+props.equipe1+'-100x100.png' : '/equipes/logo-'+props.equipe2+'-100x100.png'} alt={props.patinoire === 'home' ? props.equipe1 : props.equipe2} />
-          </Col>
-          <Col className='home-calendrier-score' xs={4}>
-            {props.score1 === '' ? 'Vs' : props.patinoire === 'home' ? props.score1 + ' - ' + props.score2 : props.score2 + ' - ' + props.score1}
-          </Col>
-          <Col className='home-calendrier-equipe-img' xs={4}>
-            <img src={props.patinoire === 'home' ? '/equipes/logo-'+props.equipe2+'-100x100.png' : '/equipes/logo-'+props.equipe1+'-100x100.png'} alt={props.patinoire === 'home' ? props.equipe2 : props.equipe1} />
-          </Col>
-        </Row>
-        <Row>
-          <Col className='home-calendrier-equipe-name' xs={4}>
-            {props.patinoire === 'home' ? props.equipe1 : props.equipe2}
-          </Col>
-          <Col className='home-calendrier-equipe-name' xs={{ span: 4, offset: 4 }}>
-            {props.patinoire === 'home' ? props.equipe2 : props.equipe1}
-          </Col>
-        </Row>
-      </Col>
-      <Col xs={12} className='home-calendrier-resultat'>
-        <span>Résultat du match : {props.resultat}</span>
-      </Col>
-    </section>
+    <Col className='home-calendrier' xs={12} md={6} lg={3}>
+      <Card border={props.resultat === 'victoire' ? 'success' : props.resultat === 'défaite' ? 'danger' : 'secondary'}>
+        <Card.Header>{gameDate}</Card.Header>
+        <Card.Body>
+          <Card.Title>
+            <Row>
+              <Col className='home-calendrier-equipe-name' xs={5}>
+                {props.patinoire === 'home' ? props.equipe1 : props.equipe2}
+              </Col>
+              <Col className='home-calendrier-equipe-name' xs={{ span: 5, offset: 2 }}>
+                {props.patinoire === 'home' ? props.equipe2 : props.equipe1}
+              </Col>
+            </Row>
+          </Card.Title>
+          <Card.Text>
+            <Row>
+              <Col className='home-calendrier-equipe-img' xs={4}>
+                <img src={props.patinoire === 'home' ? '/equipes/logo-'+props.equipe1+'-100x100.png' : '/equipes/logo-'+props.equipe2+'-100x100.png'} alt={props.patinoire === 'home' ? props.equipe1 : props.equipe2} />
+              </Col>
+              <Col className='home-calendrier-score' xs={4}>
+                {props.score1 === '' ? 'Vs' : props.patinoire === 'home' ? props.score1 + ' - ' + props.score2 : props.score2 + ' - ' + props.score1}
+              </Col>
+              <Col className='home-calendrier-equipe-img' xs={4}>
+                <img src={props.patinoire === 'home' ? '/equipes/logo-'+props.equipe2+'-100x100.png' : '/equipes/logo-'+props.equipe1+'-100x100.png'} alt={props.patinoire === 'home' ? props.equipe2 : props.equipe1} />
+              </Col>
+            </Row>
+            <Col xs={12} className={props.resultat === 'victoire' ? 'success home-calendrier-resultat' : props.resultat === 'défaite' ? 'danger home-calendrier-resultat' : 'secondary home-calendrier-resultat'}>
+              Résultat du match : <span>{props.resultat}</span>
+            </Col>
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
   )
 }
 

a26b6caf54e6502f4656b97b2e64e799a2a59c10
Fabien Cordon <fabien.cordon@ext.groupepvcp.com>
2019-10-11 11:32:41 +0200

    Fix cannot get when reloading

--- server.js
+++ server.js
@@ -46,6 +46,10 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + 'client/build/index.html'))
 })
 
+app.get('/calendrier', (req, res) => {
+  res.sendFile(path.join(__dirname + 'client/build/index.html'))
+})
+
 // START API
 
 //---->>>> GET CALENDRIER <<<<----
